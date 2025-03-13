8a1d30936a93f5fb435f9e4fd58ebcecd844e4f1
JKoriginal <92023041+JKoriginal@users.noreply.github.com>
2025-03-14 03:21:36 +0530

    avoiding cors

--- frontend/src/assets/services/api.js
+++ frontend/src/assets/services/api.js
@@ -1,15 +1,16 @@
 const API_KEY = "94c5d0693649ef5f2a28bcc31eb504f7";
-const BASE_URL = "https://www.themoviedb.org/"
+const BASE_URL = "/3"
 
 export const getPopularMovies = async () => {
     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
     const data = await response.json();
+    console.log(data);
     return data.results
 
 };
 
 export const searchMovies = async (query) => {
-    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}`)
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`)
     const data = await response.json();
     return data.results
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,38 @@
 import MovieCard from "../components/MovieCard";
-import { useState } from "react";
+import { useState , useEffect} from "react";
+import { searchMovies, getPopularMovies } from "../assets/services/api";
 import '../css/Home.css'
 
 function Home(){
 
     const [searchQuery, setSearchQuery] = useState("");
+    const[movies, setMovies] = useState([]);
+    const[error, setError] = useState(null);
+    const[loading, setLoading] = useState(true);
 
-    const movie = [
-        { id: 1, title: "Dileesha", relase_date: "2024"},
-        { id: 2, title: "Lakshan", relase_date: "2024"},
-        { id: 3, title: "Iresha", relase_date: "2024"},
-
-    ]
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+          try {
+            const popularMovies = await getPopularMovies();
+            setMovies(popularMovies);
+            console.log(popularMovies);  // Log the fetched movies
+          } catch (err) {
+            console.log(err);
+            setError("Failed to load the movie...");
+          } finally {
+            setLoading(false);
+          }
+        };
+      
+        loadPopularMovies();
+      }, []);
 
     const onSearch = (e) =>  {
         e.preventDefault()
         alert(searchQuery);
         setSearchQuery("");
     }
+
 
 
     return(
@@ -33,7 +48,7 @@ function Home(){
                 <button type = "submit" className="Search-button">Search</button>
             </form>
             <div className="movie-grid">
-                {movie.map(
+                {movies.map(
                     (movie) =>  (
                         <MovieCard  movie={movie} key={movie.id}/>
              ))}
